7ebade3a3a457f3ca2f6db61e67577b4a79e3cf7
Brandon Hubbard <bkhubbard@gmail.com>
2017-10-16 13:33:52 -0700

    started piwik library

---
+++ wp-piwik-api.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * WP Piwik API
+ *
+ * @link https://developer.piwik.org/api-reference/reporting-api
+ * @package WP-API-Libraries\WP-Piwik-API
+ */
+/*
+* Plugin Name: WP Piwik API
+* Plugin URI: https://github.com/wp-api-libraries/wp-piwik-api
+* Description: Perform API requests to Piwik in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-piwik-api
+* GitHub Branch: master
+*/
+/* Exit if accessed directly. */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/* Check if class exists. */
+if ( ! class_exists( 'PiwikAPI' ) ) {
+	
+	/**
+	 * PiwikAPI Class.
+	 */
+	class PiwikAPI {
+		
+
+	$demo_url = 'https://demo.piwik.org';
+	
+	
+	/* METADATA. */
+	
+	public function get_piwik_version() {
+		
+	}
+	
+	public function get_ip_from_header() {
+		
+	}
+	
+	public function get_settings() {
+		
+	}
+	
+	public function get_segments_metadata() {
+		
+	}
+	
+	public function get_metadata() {
+		
+	}
+	
+	public function get_report_metadata() {
+		
+	}
+	
+	public function get_processed_report() {
+		
+	}
+	
+	public function get_report_pages_metadata() {
+		
+	}
+	
+	public function get_processed_report() {
+		
+	}
+	
+	public function get_report_pages_metadata() {
+		
+	}
+	
+	public function get_processed_report() {
+		
+	}
+	
+	public function get_report_pages_metadata() {
+		
+	}
+	
+	public function get_widget_metadata() {
+		
+	}
+	
+	public function get_row_evolution() {
+		
+	}
+	
+	public function get_bulk_request() {
+		
+	}
+	
+	public function is_plugin_activated() {
+		
+	}
+	
+	public function get_suggested_values_for_segment() {
+		
+	}
+	
+	public function get_glossary_reports() {
+		
+	}
+	
+	public function get_glossary_metrics() {
+		
+	}
+	
+	
+
+	} // End Class;
+}
